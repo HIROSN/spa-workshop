e15c6866b109245a4b4a2f7b481742ffcf603bc1
Hiroshi Nagata <hirosn@live.com>
2014-12-03 18:45:32 -0800

    Update services.

--- app/js/services/geoLocationService.js
+++ app/js/services/geoLocationService.js
@@ -1,32 +1,29 @@
 'use strict';
 
-angular.module('services').factory('geoLocation', function($q, $timeout) {
-  // TODO: Enhance this service to use a GeoLocation API such as the Google maps API
-  // https://developers.google.com/maps/documentation/javascript/geocoding
+angular.module('services').factory('geoLocation',
+  ['$q', '$timeout', function($q, $timeout) {
+    return function(city) {
+      var deferred = $q.defer();
 
-  // For now just hardcoding the lat/long for our test cities
-  return function(city) {
-    var deferred = $q.defer();
-
-    // Use a $q resolved to simulate a $http call that returns a promise
-    $timeout(function() {
-      switch (city.toLowerCase()) {
-        case "seattle":
-          deferred.resolve([47.6062,-122.3321]);
-          break;
-        case "london":
-          deferred.resolve([51.5171,-0.1062]);
-          break;
-        case "tokyo":
-          deferred.resolve([35.6832,139.8089]);
-          break;
-        case "new york":
-          deferred.resolve([40.7142,-74.0064]);
-          break;
-        default:
-          deferred.reject("Invalid city");
-      }
-    }, 20);
-    return deferred.promise;
-  };
-});
+      $timeout(function() {
+        switch (city.toLowerCase()) {
+          case "seattle":
+            deferred.resolve([47.6062,-122.3321]);
+            break;
+          case "london":
+            deferred.resolve([51.5171,-0.1062]);
+            break;
+          case "tokyo":
+            deferred.resolve([35.6832,139.8089]);
+            break;
+          case "new york":
+            deferred.resolve([40.7142,-74.0064]);
+            break;
+          default:
+            deferred.reject("Invalid city");
+        }
+      }, 20);
+      return deferred.promise;
+    };
+  }]
+);
--- app/js/services/newsService.js
+++ app/js/services/newsService.js
@@ -1,31 +1,30 @@
 'use strict';
 
-angular.module('services').factory('news', function($http, $q) {
-  // Demonstrates that the proxy an be used for HTTP requests that aren't APIs, but things like RSS feeds
+angular.module('services').factory('news',
+  ['$http', '$q', function($http, $q) {
+    return function(city) {
+      var defer = $q.defer();
 
-  // To customize the parameters, see this very handy page:
-  // http://blog.slashpoundbang.com/post/12975232033/google-news-search-parameters-the-missing-manual
-  return function(city) {
-    var defer = $q.defer();
-    $http({
-      method: 'GET',
-      url: '/proxy',
-      params: {
-        url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=' +
-          'http://news.google.com/news/feeds?pz=1&cf=all&hdlOnly=1&hl=en&geo=' +
-          encodeURIComponent(city),
-        cache: true,
-        ttl: 300 // cache for 5 minutes
-      },
-      headers: {
-        accept: 'application/xml'
-      }
-    }).then(function(resp) {
-      defer.resolve(resp.data);
-    }, function(err) {
-      defer.reject(err);
-    });
+      $http({
+        method: 'GET',
+        url: '/proxy',
+        params: {
+          url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=' +
+            'http://news.google.com/news/feeds?pz=1&cf=all&hdlOnly=1&hl=en&geo=' +
+            encodeURIComponent(city),
+          cache: true,
+          ttl: 300 // cache for 5 minutes
+        },
+        headers: {
+          accept: 'application/xml'
+        }
+      }).then(function(resp) {
+        defer.resolve(resp.data);
+      }, function(err) {
+        defer.reject(err);
+      });
 
-    return defer.promise;
-  };
-});
+      return defer.promise;
+    };
+  }]
+);
