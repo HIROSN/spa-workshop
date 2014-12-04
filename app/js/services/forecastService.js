ae839513eda806ef42ca41eb84f072f222954c8c
Hiroshi Nagata <hirosn@live.com>
2014-12-03 22:34:17 -0800

    Update app.js

--- app/js/services/forecastService.js
+++ app/js/services/forecastService.js
@@ -1,18 +1,16 @@
 'use strict';
 
-angular.module('services').factory('forecast',
-  ['$q', '$http', function($q, $http) {
-    return function(lat, lng) {
-      return $http({
-        method: 'GET',
-        url: '/proxy',
-        params: {
-          url: 'https://api.forecast.io/forecast/@@FORECAST_IO_API_KEY@@/' +
-            lat + ',' + lng,
-          cache: 1,
+angular.module('services').factory('forecast', ['$http', function($http) {
+  return function(lat, lng) {
+    return $http({
+      method: 'GET',
+      url: '/proxy',
+      params: {
+        url: 'https://api.forecast.io/forecast/@@FORECAST_IO_API_KEY@@/' +
+          lat + ',' + lng,
+        cache: 1,
           ttl: 300 // cache for 5 minutes
         }
       });
-    };
-  }]
-);
+  };
+}]);
